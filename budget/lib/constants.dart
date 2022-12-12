987186de2ea9fffcb75d806f2fc1e6a6d1c017af
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2022-12-12 22:38:34 +0100

    meh

---
+++ budget/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 74, 129, 238);
+const kPrimaryLightColor = Color.fromARGB(255, 229, 194, 166);
+
+const double defaultPadding = 16.0;
